46a707f08ec78eac89a0e35ab513e2c4da8865ff
Stuart Coope <stuart@Stuarts-MBP.home>
2015-07-20 22:44:54 +0100

    User login functional

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -6,99 +6,99 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
-* @ORM\Table(name="app_users")
-* @ORM\Entity(repositoryClass="AppBundle\Entity\UserRepository")
-*/
+ * @ORM\Table(name="app_users")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\UserRepository")
+ */
 class User implements UserInterface, \Serializable
 {
-/**
-* @ORM\Column(type="integer")
-* @ORM\Id
-* @ORM\GeneratedValue(strategy="AUTO")
-*/
-private $id;
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
-/**
-* @ORM\Column(type="string", length=25, unique=true)
-*/
-private $username;
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
 
-/**
-* @ORM\Column(type="string", length=64)
-*/
-private $password;
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
 
-/**
-* @ORM\Column(type="string", length=60, unique=true)
-*/
-private $email;
+    /**
+     * @ORM\Column(type="string", length=60, unique=true)
+     */
+    private $email;
 
-/**
-* @ORM\Column(name="is_active", type="boolean")
-*/
-private $isActive;
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
 
-public function __construct()
-{
-$this->isActive = true;
+    public function __construct()
+    {
+        $this->isActive = true;
 // may not be needed, see section on salt below
 // $this->salt = md5(uniqid(null, true));
-}
+    }
 
-public function getUsername()
-{
-return $this->username;
-}
+    public function getUsername()
+    {
+        return $this->username;
+    }
 
-public function getSalt()
-{
+    public function getSalt()
+    {
 // you *may* need a real salt depending on your encoder
 // see section on salt below
-return null;
-}
+        return null;
+    }
 
-public function getPassword()
-{
-return $this->password;
-}
+    public function getPassword()
+    {
+        return $this->password;
+    }
 
-public function getRoles()
-{
-return array('ROLE_USER');
-}
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
 
-public function eraseCredentials()
-{
-}
+    public function eraseCredentials()
+    {
+    }
 
-/** @see \Serializable::serialize() */
-public function serialize()
-{
-return serialize(array(
-$this->id,
-$this->username,
-$this->password,
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
 // see section on salt below
 // $this->salt,
-));
-}
+        ));
+    }
 
-/** @see \Serializable::unserialize() */
-public function unserialize($serialized)
-{
-list (
-$this->id,
-$this->username,
-$this->password,
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
 // see section on salt below
 // $this->salt
-) = unserialize($serialized);
-}
+            ) = unserialize($serialized);
+    }
 
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
@@ -147,7 +147,7 @@ $this->password,
     /**
      * Get email
      *
-     * @return string 
+     * @return string
      */
     public function getEmail()
     {
@@ -170,7 +170,7 @@ $this->password,
     /**
      * Get isActive
      *
-     * @return boolean 
+     * @return boolean
      */
     public function getIsActive()
     {
