f94bd2d91153f3dda59682ffef8a4c55f022bb18
Stuart Coope <stuart@Stuarts-MBP.home>
2015-07-20 21:40:22 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+symblog
+=======
+
+A Symfony project created on July 18, 2015, 11:38 pm.
---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,48 @@
+<?php
+// src/AppBundle/Controller/SecurityController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+
+    /**
+     * @Route("/login", name="login_route")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'AppBundle:Security:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Unable to access this page!');
+
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+        return new Response('You are logged in');
+    }
+
+}
